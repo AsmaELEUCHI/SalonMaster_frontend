c27be1f582b91a8ae5ca571d48d88bd8e8abcb37
Asma Eleuchi <euchi.asma77@gmail.com>
2024-09-16 15:22:53 +0200

    Ajout du composant personal-space

---
+++ src/app/personal-space/personal-space.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-personal-space',
+  templateUrl: './personal-space.component.html',
+  styleUrl: './personal-space.component.css'
+})
+export class PersonalSpaceComponent {
+
+}
